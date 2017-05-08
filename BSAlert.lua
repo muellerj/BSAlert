58724984267b3eb33c7d193abb0ed4d54370ec17
Jonas Mueller <jonas@muellers.one>
2017-05-08 19:07:14 +0200

    Fix xml inclusion

--- BSAlert.lua
+++ BSAlert.lua
@@ -15,18 +15,18 @@ function BSAlert_OnLoad()
   this:RegisterEvent("PLAYER_REGEN_ENABLED");
   this:RegisterEvent("PLAYER_REGEN_DISABLED");
   this:RegisterEvent("PLAYER_AURAS_CHANGED");
-  
+
   -- initialize combat tracker
   BSAlert_combat = nil;
-  
+
   -- define the border as "backdrop"
   local backdrop = {
     edgeFile = "Interface\\AddOns\\BSAlert\\border", edgeSize = 64,
     insets = {left = 64, right = 64, top = 64, bottom = 64},
   }
-  
+
   -- build the frame
-  local BSAlert = CreateFrame("Frame")
+  BSAlert = CreateFrame("Frame")
   BSAlert:SetFrameStrata("BACKGROUND")
   BSAlert:SetWidth(GetScreenWidth() * UIParent:GetEffectiveScale())
   BSAlert:SetHeight(GetScreenHeight() * UIParent:GetEffectiveScale())
@@ -39,7 +39,7 @@ function BSAlert_OnEvent(event)
   if     (event == "PLAYER_REGEN_DISABLED") then BSAlert_combat = true;
   elseif (event == "PLAYER_REGEN_ENABLED")  then BSAlert_combat = nil;
   end
-  
+
   if BSAlert_combat and (not HasBuff("player", "Ability_Warrior_BattleShout")) then
     BSAlert:Show();
   else
