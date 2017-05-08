f176379b6995ce16431eafa7cb0867d72f323732
Jonas Müller <muellerj@users.noreply.github.com>
2017-05-08 11:01:29 +0200

    Change functions to reference xml

--- BSAlert.lua
+++ BSAlert.lua
@@ -1,20 +1,3 @@
--- define the border as "backdrop"
-local backdrop = {
-  edgeFile = "Interface\\AddOns\\BSAlert\\border", edgeSize = 64,
-  insets = {left = 64, right = 64, top = 64, bottom = 64},
-}
-
-local BSAlert_combat = nil;
-
--- build the frame
-local BSAlert = CreateFrame("Frame")
-BSAlert:SetFrameStrata("BACKGROUND")
-BSAlert:SetWidth(GetScreenWidth() * UIParent:GetEffectiveScale())
-BSAlert:SetHeight(GetScreenHeight() * UIParent:GetEffectiveScale())
-BSAlert:SetBackdrop(backdrop)
-BSAlert:SetPoint("CENTER",0,0)
-BSAlert:Hide()
-
 function HasBuff(unit, texturename)
   local id = 1;
   while (UnitBuff(unit, id)) do
@@ -27,19 +10,39 @@ function HasBuff(unit, texturename)
   return nil;
 end
 
--- register for events
-BSAlert:RegisterEvent("PLAYER_REGEN_ENABLED");
-BSAlert:RegisterEvent("PLAYER_REGEN_DISABLED");
-BSAlert:RegisterEvent("PLAYER_AURAS_CHANGED");
+function BSAlert_OnLoad()
+  -- hook into events
+	this:RegisterEvent("PLAYER_REGEN_ENABLED");
+	this:RegisterEvent("PLAYER_REGEN_DISABLED");
+  this:RegisterEvent("PLAYER_AURAS_CHANGED");
+  
+  -- initialize combat tracker
+  BSAlert_combat = nil;
+  
+  -- define the border as "backdrop"
+  local backdrop = {
+    edgeFile = "Interface\\AddOns\\BSAlert\\border", edgeSize = 64,
+    insets = {left = 64, right = 64, top = 64, bottom = 64},
+  }
+  
+  -- build the frame
+  local BSAlert = CreateFrame("Frame")
+  BSAlert:SetFrameStrata("BACKGROUND")
+  BSAlert:SetWidth(GetScreenWidth() * UIParent:GetEffectiveScale())
+  BSAlert:SetHeight(GetScreenHeight() * UIParent:GetEffectiveScale())
+  BSAlert:SetBackdrop(backdrop)
+  BSAlert:SetPoint("CENTER",0,0)
+  BSAlert:Hide()
+end
 
--- show/hide on combat
-BSAlert:SetScript("OnEvent", function(event)
-  if (event == "PLAYER_REGEN_DISABLED") then BSAlert_combat = true; end
-  if (event == "PLAYER_REGEN_ENABLED")  then BSAlert_combat = nil; end
-
+function BSAlert_OnEvent(event)
+	if     (event == "PLAYER_REGEN_DISABLED") then BSAlert_combat = true;
+	elseif (event == "PLAYER_REGEN_ENABLED")  then BSAlert_combat = nil;
+	end
+  
   if BSAlert_combat and (not HasBuff("player", "Ability_Warrior_BattleShout")) then
     BSAlert:Show();
   else
     BSAlert:Hide();
   end
-end)
+end
