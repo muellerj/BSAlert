6f6d8cf17a90ee2187a7863edf94d74cc5162e5d
Jonas Mueller <jonas@muellers.one>
2017-05-08 07:33:52 +0200

    Only show alert when in combat
    (untested)

--- BSAlert.lua
+++ BSAlert.lua
@@ -26,13 +26,21 @@ function HasBuff(unit, texturename)
 end
 
 -- register for events
-BSAlert:RegisterEvent("PLAYER_AURAS_CHANGED")
+BSAlert:RegisterEvent("PLAYER_REGEN_ENABLED");
+BSAlert:RegisterEvent("PLAYER_REGEN_DISABLED");
+BSAlert:RegisterEvent("PLAYER_AURAS_CHANGED");
 
 -- show/hide on combat
-BSAlert:SetScript("OnEvent", function()
-  if HasBuff("player", "Ability_Warrior_BattleShout") then
-		BSAlert:Hide()
+BSAlert:SetScript("OnEvent", function(event)
+  if (event == "PLAYER_REGEN_DISABLED") then
+    BSAlert_combat = true;
+  elseif (event == "PLAYER_REGEN_ENABLED") then
+    BSAlert_combat = nil;
+  end
+
+  if BSAlert_combat and (not HasBuff("player", "Ability_Warrior_BattleShout")) then
+    BSAlert:Show();
   else
-		BSAlert:Show()
+    BSAlert:Hide();
   end
 end)
